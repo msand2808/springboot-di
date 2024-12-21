07cbfdaf9b42bf2278b1fdbabd71087745aad7c4
Misael <misael.sandoval2808@gmail.com>
2024-12-21 12:27:11 -0600

    se agrego el archivo someController y se agrego un ejemplo sencillo de inmutabilidad de informacion, luego se corrigio un leve error de logica en el productservice, a la hora de ponerle el precio con impuesto al producto

--- src/main/java/com/misael/springboot/di/app/springboot_di/services/ProductService.java
+++ src/main/java/com/misael/springboot/di/app/springboot_di/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.misael.springboot.di.app.springboot_di.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.misael.springboot.di.app.springboot_di.models.Product;
 import com.misael.springboot.di.app.springboot_di.repositories.ProductRepository;
@@ -11,7 +12,11 @@ public class ProductService {
 
     public List<Product> findAll()
     {
-        return repository.findAll();
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.25d;
+            Product newProd = new Product(p.getId(), p.getName(), priceImp.longValue());
+            return newProd;
+        }).collect(Collectors.toList());
     }
     
     public Product findById(Long id)
